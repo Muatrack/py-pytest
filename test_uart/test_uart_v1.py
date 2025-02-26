17b49bfae18d44e908cb4fe695ca599c3a52cf51
Muatrack <fjfhccfkuk@outlook.com>
2025-02-26 14:03:59 +0800

    update

--- test_uart/test_uart_v1.py
+++ test_uart/test_uart_v1.py
@@ -1,6 +1,16 @@
 import pytest
 # import ../components/uart/uart.py
 
+@pytest.fixture
+def run_fixture():
+    print("Before testing")
+
+    yield
+
+    print("After testing")
+
 @pytest.mark.uart
-def test_uart_write():
+# @pytest.fixture("run_fixture")
+def test_uart_write(run_fixture):
+# def test_uart_write():
     print("I am pstest tcase, uart modules")
