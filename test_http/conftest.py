53c563c9c943b06ea546c967ecd81b06ee1e72a2
Muatrack <fjfhccfkuk@outlook.com>
2025-02-26 19:22:54 +0800

    update

--- test_http/conftest.py
+++ test_http/conftest.py
@@ -1,7 +1,24 @@
 import pytest
+import requests
+# import response
 
+class Http:
+    def connect(self):
+        print("conn http conn")
+
+    def response_cb_set(selv):
+        print("set http resp cb")
+
+    def close(self):
+        print("close http conn")
+    
+@pytest.fixture()
+def httpObj() -> object:
+    return "http class object"
+
+# @pytest.mark.usefixtures('httpObj')
 @pytest.fixture(scope='module')
 def http_fixture_module() -> any :
-    print("\n[ Gonna open http connection ] ---------------")
+    print("\n[ Gonna open http connection ] --------------- str:", httpObj)    
     yield
     print("\n[ Close http connection ] ---------------")
