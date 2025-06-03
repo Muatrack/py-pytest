a8f519bdbe20f879b1c3ccf9ad39612c46169ab0
sernola <fjfhccfkuk@163.com>
2025-06-03 20:34:46 +0800

    update

--- elc_switcher_perform.py
+++ elc_switcher_perform.py
@@ -11,21 +11,10 @@ import time
 _httpClient = client.Client()
 _platform   = ElcPt.Platform(_httpClient)
 
-def turnSlaveOn(gwSn:str, slvSn:str):
-    _platform.gatewaySelect("E10132E4952250A3")
-    _platform.slaveSelect("150802DB9422015F")
-    resp = _platform.slaveTrunOff()
-    time.sleep(5)
-    resp = _platform.slaveTrunOn()
-    time.sleep(5)
-    resp = _platform.slaveTrunOff()
-    time.sleep(5)
-    resp = _platform.slaveTrunOn()
-    time.sleep(5)
-    resp = _platform.slaveTrunOff()
-    time.sleep(5)
-    resp = _platform.slaveTrunOn()
-
-
 if __name__ == '__main__':
-    turnSlaveOn('', '')
+    
+    while( True ):        
+        resp = _platform.slaveTrunOff()
+        time.sleep(5)
+        resp = _platform.slaveTrunOn()
+        time.sleep(5)
