5c085da81e2e8f2a32da0eb2600d3a651a55bf22
sernola <fjfhccfkuk@163.com>
2025-06-03 09:51:49 +0800

    将 python 日志输出至 /tmp/t.log

--- main_elc_gw_modbus_04.py
+++ main_elc_gw_modbus_04.py
@@ -1,7 +1,10 @@
 #!/home/private/projects/pytest/demo/py-pytest/.env/bin/python3
 
 import pytest
+import sys
 
-pytest.main(['-m', 'http_api_elc_gateway_modbus', '--repeat-scope=class', "--count=100000"])
+sys.stdout("/tmp/t.log", "w")
+sys.stderr = sys.stdout
+pytest.main(['-m', 'http_api_elc_gateway_modbus', '--repeat-scope=class', "--count=10000"])
 # pytest.main(['-vs', '-m', 'http_api_elc_gateway_modbus', '--repeat-scope=class', "--count=1000"])
 # pytest.main(['-xvs', '-m', 'http_api_elc_gateway_modbus', '--repeat-scope=class', "--count=1000"])
