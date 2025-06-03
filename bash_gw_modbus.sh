093663b6200b6567801a3a7f256a5798ce4ee70a
Muatrack <fjfhccfkuk@outlook.com>
2025-06-03 10:26:34 +0800

    add bash script to run python-gw-modbus test

---
+++ bash_gw_modbus.sh
@@ -0,0 +1,1 @@
+nohup  python main_elc_gw_modbus_04.py > /tmp/t.log 2>&1 &
--- main_elc_gw_modbus_04.py
+++ main_elc_gw_modbus_04.py
@@ -1,10 +1,7 @@
 #!/home/private/projects/pytest/demo/py-pytest/.env/bin/python3
 
 import pytest
-import sys
 
-sys.stdout("/tmp/t.log", "w")
-sys.stderr = sys.stdout
-pytest.main(['-m', 'http_api_elc_gateway_modbus', '--repeat-scope=class', "--count=10000"])
+pytest.main(['--tb=no','-m', 'http_api_elc_gateway_modbus', '--repeat-scope=class', "--count=10000"])
 # pytest.main(['-vs', '-m', 'http_api_elc_gateway_modbus', '--repeat-scope=class', "--count=1000"])
 # pytest.main(['-xvs', '-m', 'http_api_elc_gateway_modbus', '--repeat-scope=class', "--count=1000"])
